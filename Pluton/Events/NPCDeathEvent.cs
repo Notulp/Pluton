23621798109204a11085120260ce8403c7327707
balu92 <balugyanszky.gabor@gmail.com>
2014-09-20 01:11:39 +0200

    NPCs

---
+++ Pluton/Events/NPCDeathEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pluton.Events {
+	public class NPCDeathEvent : HurtEvent {
+
+		private NPC _victim;
+		private HitInfo _info;
+
+		public NPCDeathEvent(NPC npc, HitInfo info) : base(info){
+			_victim = npc;
+			_info = info;
+		}
+
+		public NPC Victim {
+			get {
+				return _victim;
+			}
+		}
+	}
+}
+
---
+++ Pluton/Events/NPCHurtEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pluton.Events {
+	public class NPCHurtEvent : HurtEvent {
+
+		private NPC _victim;
+		private HitInfo _info;
+
+		public NPCHurtEvent(NPC npc, HitInfo info) : base(info){
+			_victim = npc;
+			_info = info;
+		}
+
+		public NPC Victim {
+			get {
+				return _victim;
+			}
+		}
+	}
+}
+
