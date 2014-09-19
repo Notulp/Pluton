dec0a896aa4cfce40b166f2a46e78c99fabd58d5
balu92 <balugyanszky.gabor@gmail.com>
2014-09-19 14:47:29 +0200

    More to the player

--- Pluton/Player.cs
+++ Pluton/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Pluton {
 	public class Player {
@@ -9,20 +10,41 @@ namespace Pluton {
 			basePlayer = player;
 		}
 
+		public static Player Find(string nameOrSteamidOrIP) {
+			return new Player(BasePlayer.Find(nameOrSteamidOrIP));
+		}
+
+		public static Player FindByGameID(ulong steamID) {
+			return new Player(BasePlayer.FindByID(steamID));
+		}
+
+		public static Player FindBySteamID(string steamID) {
+			return new Player(BasePlayer.FindByID(UInt64.Parse(steamID)));
+		}
+
+		public void Kick(string reason = "") {
+			Network.Net.sv.Kick(basePlayer.net.connection, reason);
+		}
+
 		public void Kill() {
 			basePlayer.StartDead();
 		}
 
-
-		public string Name {
+		public bool Admin {
 			get {
-				return basePlayer.displayName;
+				return basePlayer.IsAdmin();
 			}
 		}
 
-		public string SteamID {
+		public string AuthStatus {
 			get {
-				return basePlayer.userID.ToString();
+				return basePlayer.net.connection.authStatus;
+			}
+		}
+
+		public ulong GameID {
+			get {
+				return basePlayer.userID;
 			}
 		}
 
@@ -32,9 +54,75 @@ namespace Pluton {
 			}
 		}
 
-		public ulong GameID {
+		public string IP {
 			get {
-				return basePlayer.userID;
+				return basePlayer.net.connection.ipaddress;
+			}
+		}
+
+		public Vector3 Location {
+			get {
+				return basePlayer.transform.position;
+			}
+			set {
+				basePlayer.transform.position.Set(value.x, value.y, value.z);
+			}
+		}
+
+		public string Name {
+			get {
+				return basePlayer.displayName;
+			}
+		}
+
+		public string OS {
+			get {
+				return basePlayer.net.connection.os;
+			}
+		}
+
+		public int Ping {
+			get {
+				return basePlayer.net.connection.ping;
+			}
+		}
+
+		public string SteamID {
+			get {
+				return basePlayer.userID.ToString();
+			}
+		}
+
+		public float TimeOnline {
+			get {
+				return basePlayer.net.connection.connectionTime;
+			}
+		}
+
+		public float X {
+			get {
+				return basePlayer.transform.position.x;
+			}
+			set {
+				basePlayer.transform.position.Set(value, Y, Z);
+			}
+		}
+
+		public float Y {
+			get {
+				return basePlayer.transform.position.y;
+			}
+			set {
+				basePlayer.transform.position.Set(X, value, Z);
+			}
+		}
+
+		public float Z {
+			get {
+				return basePlayer.transform.position.z;
+			}
+			set {
+				basePlayer.transform.position.Set(X, Y, value);
 			}
 		}
 	}
