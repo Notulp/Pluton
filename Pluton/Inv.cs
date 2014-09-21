d345e61ac9919e386503405a0239cd2a61167de4
balu92 <balugyanszky.gabor@gmail.com>
2014-09-21 20:38:12 +0200

    Inv, invitem init

---
+++ Pluton/Inv.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Pluton {
+	public class Inv {
+
+		private readonly PlayerInventory _inv;
+
+		public Inv (PlayerInventory inv) {
+			_inv = inv;
+		}
+
+		public bool Add(InvItem item) {
+			return _inv.GiveItem(item._item);
+		}
+
+		public bool Add(InvItem item, ItemContainer con) {
+			return _inv.GiveItem(item._item, con);
+		}
+
+		public bool Add(int itemID) {
+			return Add(itemID, 1);
+		}
+
+		public bool Add(int itemID, int amount) {
+			return _inv.GiveItem(itemID, amount, true);
+		}
+
+		public ItemContainer InnerBelt() {
+			return _inv.containerBelt;
+		}
+
+		public ItemContainer InnerMain() {
+			return _inv.containerMain;
+		}
+
+		public ItemContainer InnerWear() {
+			return _inv.containerWear;
+		}
+
+		public List<InvItem> AllItems() {
+			return (from item in _inv.AllItems()
+				select new InvItem(item)).ToList();
+		}
+
+		public List<InvItem> BeltItems() {
+			return (from item in _inv.containerBelt.itemList
+				select new InvItem(item)).ToList();
+		}
+
+		public List<InvItem> MainItems() {
+			return (from item in _inv.containerMain.itemList
+				select new InvItem(item)).ToList();
+		}
+
+		public List<InvItem> WearItems() {
+			return (from item in _inv.containerWear.itemList
+				select new InvItem(item)).ToList();
+		}
+	}
+}
+
---
+++ Pluton/InvItem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pluton {
+	public class InvItem {
+
+		public Item _item;
+
+		public InvItem(string name, int amount) {
+			var item = ItemManager.CreateByName(name, amount);
+			if (item == null) {
+				Logger.LogDebug(String.Format("[InvItem] Couldn't create item: {0}x{1}", amount, name));
+				_item = null;
+			}
+			_item = item;
+		}
+
+		public InvItem(string name) {
+			var item = ItemManager.CreateByName(name, 1);
+			if (item == null) {
+				Logger.LogDebug(String.Format("[InvItem] Couldn't create item: {0}x{1}", 1, name));
+				_item = null;
+			}
+			_item = item;
+		}
+
+		public InvItem(Item item) {
+			_item = item;
+		}
+
+		public bool CanStack(InvItem item){
+			return _item.CanStack(item._item);
+		}
+
+		public void Drop(Transform position, Vector3 offset) {
+			_item.Drop(position, offset);
+		}
+
+		public Entity Instantiate(Vector3 v3, Quaternion q) {
+			return new Entity(_item.CreateWorldObject(v3, q));
+		}
+
+		public string Name {
+			get {
+				return _item.info.displayname;
+			}
+		}
+
+		public int Quantity {
+			get {
+				return _item.amount;
+			}
+			set {
+				_item.amount = value;
+			}
+		}
+
+		public int Slot {
+			get {
+				return _item.position;
+			}
+			set {
+				_item.position = value;
+			}
+		}
+	}
+}
+
