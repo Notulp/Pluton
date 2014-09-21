44e5b9ed5cac8596d630cbf8e9706655884e4ca6
balu92 <balugyanszky.gabor@gmail.com>
2014-09-21 23:52:59 +0200

    directoryconfig is no more

--- Pluton/Config.cs
+++ Pluton/Config.cs
@@ -7,17 +7,10 @@ using UnityEngine;
 
 namespace Pluton {
 	public class Config {
-		public static IniParser PlutonDirectoryConfig;
 		public static IniParser PlutonConfig;
 
-		public static void Init(string DirectoryConfigPath) {
-			if (File.Exists(DirectoryConfigPath)) {
-				PlutonDirectoryConfig = new IniParser(DirectoryConfigPath);
-				Debug.Log("DirectoryConfig " + DirectoryConfigPath + " loaded!");
-			}
-			else Debug.Log("DirectoryConfig " + DirectoryConfigPath + " NOT loaded!");
-
-			string ConfigPath = Path.Combine(GetPublicFolder(), "Pluton.cfg");
+		public static void Init() {
+			string ConfigPath = Path.Combine(Util.GetPublicFolder(), "Pluton.cfg");
 
 			if (File.Exists(ConfigPath)) {
 				PlutonConfig = new IniParser(ConfigPath);
@@ -33,18 +26,6 @@ namespace Pluton {
 		public static bool GetBoolValue(string Section,string Setting) {
 			var val = PlutonConfig.GetSetting(Section, Setting);
 			return val != null && val.ToLower() == "true";
-		}
-
-		public static string GetModulesFolder() {
-			Regex root = new Regex(@"^%RootFolder%", RegexOptions.IgnoreCase);             
-			string path = root.Replace(PlutonDirectoryConfig.GetSetting("Settings", "ModulesFolder"), Util.GetRootFolder()) + @"\";
-			return Util.NormalizePath(path);
-		}
-
-		public static string GetPublicFolder() {
-			Regex root = new Regex(@"^%RootFolder%", RegexOptions.IgnoreCase);             
-			string path = root.Replace(PlutonDirectoryConfig.GetSetting("Settings", "PublicFolder"), Util.GetRootFolder()) + @"\";
-			return Util.NormalizePath(path);
 		}
 	}
 }
