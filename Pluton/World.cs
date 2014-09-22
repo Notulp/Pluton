f9fe1b4833b79e31d89543153cf5f61c01589dca
balu92 <balugyanszky.gabor@gmail.com>
2014-09-22 15:16:14 +0200

    World class init

---
+++ Pluton/World.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pluton {
+	public class World {
+
+		private static World _world;
+
+		public void AirDrop() {
+			BaseEntity entity = GameManager.CreateEntity("events/cargo_plane", new Vector3(), new Quaternion());
+			if (!(bool) ((UnityEngine.Object) entity))
+				return;
+			entity.Spawn(true);
+		}
+
+		public void AirDropAtPlayer(Player player) {
+			BaseEntity entity = GameManager.CreateEntity("events/cargo_plane", player.Location, player.basePlayer.transform.rotation);
+			if (!(bool) ((UnityEngine.Object) entity))
+				return;
+			entity.Spawn(true);
+		}
+
+		public static World GetWorld() {
+			return _world;
+		}
+
+		public World() {
+			_world = this;
+		}
+	}
+}
+
