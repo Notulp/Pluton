b4a2feeb9565ce483a4543fafb58f251f9e1979d
balu92 <balugyanszky.gabor@gmail.com>
2014-09-25 00:57:11 +0200

    GetGround

--- Pluton/World.cs
+++ Pluton/World.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Pluton {
 	public class World {
 
-		private static World _world;
+		private static World instance;
 
 		public void AirDrop() {
 			BaseEntity entity = GameManager.CreateEntity("events/cargo_plane", new Vector3(), new Quaternion());
@@ -20,12 +20,24 @@ namespace Pluton {
 			entity.Spawn(true);
 		}
 
-		public static World GetWorld() {
-			return _world;
+		public float GetGround(float x, float z) {
+			RaycastHit hit;
+			var origin = new Vector3(x, 5000, z);
+			float ground = 0f;
+			if (Physics.Raycast(origin, Vector3.down, out hit, 8000, Physics.AllLayers)) {
+				ground = hit.point.y;
+			}
+			return ground;
 		}
 
-		public World() {
-			_world = this;
+		public float GetGround(Vector3 v3) {
+			return GetGround(v3.x, v3.z);
+		}
+
+		public static World GetWorld() {
+			if (instance == null)
+				instance = new World();
+			return instance;
 		}
 	}
 }
