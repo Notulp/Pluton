842257745b56cd83c710b98e694755f7284c5a31
balu92 <balugyanszky.gabor@gmail.com>
2014-09-25 00:53:26 +0200

    Loadout

---
+++ Pluton/Objects/LoadOutItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pluton {
+	public class LoadOutItem {
+
+		public readonly int Amount;
+		public readonly string Name;
+
+		public LoadOutItem(string name) {
+			Amount = 1;
+			Name = name;
+		}
+
+		public LoadOutItem(string name, int amount) {
+			Amount = amount;
+			Name = name;
+		}
+
+		public InvItem invItem {
+			get {
+				return new InvItem(Name, Amount);
+			}
+		}
+	}
+}
+
