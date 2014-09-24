84a9208c74bef4d21cdeed612398392324d538c1
balu92 <balugyanszky.gabor@gmail.com>
2014-09-25 00:54:28 +0200

    ChatString instead of Consolesys.Arg

---
+++ Pluton/Objects/ChatString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pluton {
+	public class ChatString {
+
+		public readonly ConsoleSystem.Arg _arg;
+		public readonly string OriginalText;
+		public string BroadcastName;
+		public string FinalText;
+		public string ReplyWith;
+
+		public ChatString(ConsoleSystem.Arg args) {
+			_arg = args;
+			if (args.FromClient)
+				BroadcastName = args.connection.username;
+			else
+				BroadcastName = Server.server_message_name;
+			OriginalText = args.ArgsStr;
+			FinalText = args.ArgsStr;
+			ReplyWith = "chat.say was executed";
+		}
+	}
+}
+
