bc453a31b0b070757a9084b495c3a0e78eab4996
balu92 <balugyanszky.gabor@gmail.com>
2014-10-01 00:46:01 +0200

    Assembly info

--- Pluton/Properties/AssemblyInfo.cs
+++ Pluton/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("Pluton")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("Rust reboot server mod")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
+[assembly: AssemblyProduct ("Pluton")]
 [assembly: AssemblyCopyright ("balu")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyVersion ("0.9.2.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
