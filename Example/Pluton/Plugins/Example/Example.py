79eb9241ac8ce36b14512287bc473a426db50cf1
dretax <dretax@users.noreply.github.com>
2014-10-06 17:06:36 +0200

    Use elif to make it faster.

--- Example/Pluton/Plugins/Example/Example.py
+++ Example/Pluton/Plugins/Example/Example.py
@@ -25,11 +25,11 @@ class Example:
 				if(Server.LoadOuts.ContainsKey(cmd.quotedArgs[0])):
 					loadout = Server.LoadOuts[cmd.quotedArgs[0]]
 					loadout.ToInv(cmd.User.Inventory)
-			if(cmd.cmd == "apple"):
+			elif(cmd.cmd == "apple"):
 				cmd.User.Message("An apple a day keeps the doctor away!")
 				item = InvItem("Apple")
 				item.Instantiate(Vector3(cmd.User.X + 3, cmd.User.Y + 3, cmd.User.Z + 3))
-			if(cmd.cmd == "help"):
+			elif(cmd.cmd == "help"):
 				cmd.User.Message("Usable command: /whereami, /kit starter")
 		except:
 			Debug.Log(String.Format("Something went wrong while executing: /{0} args", cmd.cmd, String.Join(" ", cmd.args)))
