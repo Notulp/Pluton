dcbd638acf8ed6d5ba09ce1974689b2e64ace3d7
balu92 <balugyanszky.gabor@gmail.com>
2014-10-20 00:30:49 +0200

    this is the proficiency we need

--- Pluton/Events/BuildingEvent.cs
+++ Pluton/Events/BuildingEvent.cs
@@ -10,18 +10,17 @@ namespace Pluton.Events
         public BuildingPart BuildingPart;
         public readonly Player Builder;
         public string BlockFullName;
-        public float Proficiency;
         public string BlockName;
         public InvItem Tool;
+        public readonly HitInfo _info;
 
         public BuildingEvent(BuildingPart bp, HitInfo info)
         {
-
+            _info = info;
             BasePlayer player = info.Initiator as BasePlayer;
             var p = new Player(player);
             Tool = new InvItem(info.Weapon);
             _block = bp.buildingBlock;
-            Proficiency = info.resourceGatherProficiency;
             BuildingPart = bp;
             Builder = p;
 
@@ -29,6 +28,16 @@ namespace Pluton.Events
                 BlockName = _block.blockDefinition.name;
                 Sockets = _block.blockDefinition.sockets;
                 BlockFullName = _block.blockDefinition.fullname;
+            }
+        }
+
+
+        public float[] Proficiency {
+            get {
+                return _info.buildProficiency;
+            }
+            set {
+                _info.buildProficiency = value;
             }
         }
     }
