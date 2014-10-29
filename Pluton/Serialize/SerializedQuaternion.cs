d64afad1f5d63c95061f6a62097920b2ee7ee126
balu92 <balugyanszky.gabor@gmail.com>
2014-10-29 18:15:24 +0100

    Serialized vector3, quat

---
+++ Pluton/Serialize/SerializedQuaternion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pluton
+{
+    [Serializable]
+    public class SerializedQuaternion : CountedInstance
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public SerializedQuaternion(Quaternion q)
+        {
+            x = q.x;
+            y = q.y;
+            z = q.z;
+            w = q.w;
+        }
+
+        public Quaternion ToQuaternion()
+        {
+            return new Quaternion(x, y, z, w);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0:F1}, {1:F1}, {2:F1}, {3:F1})", new object[] { x, y, z, w });
+        }
+    }
+}
+
---
+++ Pluton/Serialize/SerializedVector3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pluton
+{
+    [Serializable]
+    public class SerializedVector3 : CountedInstance
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public SerializedVector3(Vector3 v3)
+        {
+            x = v3.x;
+            y = v3.y;
+            z = v3.z;
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3(x, y, z);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0:F1}, {1:F1}, {2:F1})", x, y, z);
+        }
+    }
+}
+
