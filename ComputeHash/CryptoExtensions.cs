675cc8ef742f565460f8bd7dac64324e9076fbba
balu92 <balugyanszky.gabor@gmail.com>
2014-11-10 12:59:24 +0100

    quick console app to generate hashes

---
+++ ComputeHash/CryptoExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ComputeHash
+{
+	public static class CryptoExtensions
+	{
+		public static string GetMD5Hash(this byte[] input)
+		{
+			using (MD5 md5hash = MD5.Create()) {
+				return GetMD5Hash(md5hash, input);
+			}
+		}
+
+		public static string GetMD5Hash(MD5 md5Hash, byte[] input)
+		{
+			byte[] data = md5Hash.ComputeHash(input);
+
+			StringBuilder sBuilder = new StringBuilder();
+
+			for (int i = 0; i < data.Length; i++)
+			    sBuilder.Append(data[i].ToString("x2"));
+
+			return sBuilder.ToString();
+		}
+	}
+}
+
---
+++ ComputeHash/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace ComputeHash
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Hashes.txt");
+
+			string[] lines;
+			if (File.Exists(path))
+				lines = File.ReadAllLines(path);
+			else
+				lines = new string[]{};
+
+			foreach (string arg in args) {
+				string hash = File.ReadAllBytes(arg).GetMD5Hash();
+
+				bool contains = false;
+				foreach (string line in lines) {
+					if (line.StartsWith("//"))
+						continue;
+					if (line.Equals(hash)) {
+						contains = true;
+						break;
+					}
+				}
+
+				if (!contains) {
+					File.AppendAllText(path, String.Format("// {0}\r\n{1}\r\n\r\n", Path.GetFileName(arg), hash));
+					Console.WriteLine("The file's hash has been successfully added to: " + path);
+				} else {
+					Console.WriteLine("The file's hash is already added to: " + path);
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
