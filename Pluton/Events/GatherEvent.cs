f484aed434949b75ef86be08ec7d5141f7a36569
balu92 <balugyanszky.gabor@gmail.com>
2014-12-26 13:46:51 +0100

    GatherEvent

--- Pluton/Events/GatherEvent.cs
+++ Pluton/Events/GatherEvent.cs
@@ -9,7 +9,6 @@ namespace Pluton.Events
         public readonly BaseResource _res;
         public readonly string Prefab;
         public readonly uint PrefabID;
-        public readonly InvItem Tool;
         public readonly int Stage;
 
         public GatherEvent(BaseResource res, HitInfo info)
@@ -20,25 +19,74 @@ namespace Pluton.Events
             Prefab = _res.LookupPrefabName();
             PrefabID = _res.prefabID;
             Stage = _res.stage;
-            //if (_info.Weapon != null)
-            //    Tool = new InvItem(_info.Weapon);
-
         }
 
-        /*public float DamageAmount {
+        /******************
+        *                 *
+        * Generic      0  *
+        * Hunger       1  *
+        * Thirst       2  *
+        * Cold         3  *
+        * Drowned      4  *
+        * Heat         5  *
+        * Bleeding     6  *
+        * Poison       7  *
+        * Suicide      8  *
+        * Bullet       9  *
+        * Slash        10 *
+        * Blunt        11 *
+        * Fall         12 *
+        * Radiation    13 *
+        * Bite         14 *
+        * Stab         15 *
+        *                 *
+        ******************/
+
+        public float[] DamageAmounts {
             get {
-                return _info.damageAmount;
+                return _info.damageTypes.types;
             }
             set {
-                _info.damageAmount = value;
+                _info.damageTypes.types = value;
             }
         }
 
         public Rust.DamageType DamageType {
             get {
-                return _info.damageType;
+                return _info.damageTypes.GetMajorityDamageType();
             }
-        }*/
+        }
+
+        public Entity Attacker {
+            get {
+                try {
+                    if (_info.Initiator != null)
+                        return new Entity(_info.Initiator);
+                    return null;
+                } catch (Exception ex) {
+                    Logger.LogWarning("[HurtEvent] Got an exception instead of the attacker.");
+                    Logger.LogException(ex);
+                    return null;
+                }
+            }
+        }
+
+        public InvItem Weapon {
+            get {
+                try {
+                    if (_info.Weapon == null)
+                        return null;
+                    if (_info.Weapon.GetItem() == null)
+                        return null;
+
+                    return new InvItem(_info.Weapon.GetItem());
+                } catch (Exception ex) {
+                    Logger.LogWarning("[HurtEvent] Got an exception instead of the weapon.");
+                    Logger.LogException(ex);
+                    return null;
+                }
+            }
+        }
 
         public float Health {
             get {
