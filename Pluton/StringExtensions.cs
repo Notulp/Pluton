1d2ed1ce62936f0499675f4f5393302da73a2059
balu92 <balugyanszky.gabor@gmail.com>
2015-02-11 23:41:32 +0100

    More StringEX

--- Pluton/StringExtensions.cs
+++ Pluton/StringExtensions.cs
@@ -4,9 +4,29 @@ namespace Pluton
 {
     public static class StringExtensions
     {
+        public static string BoldText(this string self)
+        {
+            return String.Format("<b>self</b>", self);
+        }
+
         public static string ColorText(this string self, string color)
         {
             return String.Format("<color=#{0}>{1}</color>", color, self);
+        }
+
+        public static string ItalicText(this string self)
+        {
+            return String.Format("<i>self</i>", self);
+        }
+
+        public static string SetSize(this string self, int size)
+        {
+            return String.Format("<size={0}>{1}</size>", size, self);
+        }
+
+        public static string SetSize(this string self, string size)
+        {
+            return String.Format("<size={0}>{1}</size>", size, self);
         }
 
         public static string QuoteSafe(this string self)
