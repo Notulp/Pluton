9de5bd3dc168314c17ef95016fbde9115d30eb3f
balu92 <balugyanszky.gabor@gmail.com>
2015-02-28 00:45:13 +0100

    Find more!

--- Pluton/Find.cs
+++ Pluton/Find.cs
@@ -20,6 +20,48 @@ namespace Pluton
             }
         }
 
+        public List<BuildingPart> BuildingParts()
+        {
+            return (from block in UnityEngine.Object.FindObjectsOfType<BuildingBlock>()
+                select new BuildingPart(block)).ToList<BuildingPart>();
+        }
+
+        public List<BuildingPart> BuildingPartsByName(string name)
+        {
+            return (from block in UnityEngine.Object.FindObjectsOfType<BuildingBlock>()
+                where block.LookupPrefabName() == name ||
+                block.LookupShortPrefabName() == name ||
+                block.blockDefinition.fullname == name
+                select new BuildingPart(block)).ToList<BuildingPart>();
+        }
+
+        public List<BuildingPart> BuildingPartsByGrade(int grade)
+        {
+            return (from block in UnityEngine.Object.FindObjectsOfType<BuildingBlock>()
+                where block.grade == grade
+                select new BuildingPart(block)).ToList<BuildingPart>();
+        }
+
+        public List<Entity> CupBoards()
+        {
+            return (from board in UnityEngine.Object.FindObjectsOfType<BuildingPrivlidge>()
+                select new Entity(board)).ToList<Entity>();
+        }
+
+        // not tested...
+        /*public List<Entity> CupBoards(ulong steamid)
+        {
+            return (from board in UnityEngine.Object.FindObjectsOfType<BuildingPrivlidge>()
+                where board.authorizedPlayers.Where(x => x.userid == steamid).Count() != 0
+                select new Entity(board)).ToList<Entity>();
+        }*/
+
+        public List<Entity> Doors()
+        {
+            return (from door in UnityEngine.Object.FindObjectsOfType<Door>()
+                select new Entity(door as BaseEntity)).ToList<Entity>();
+        }
+
         /// <summary>
         /// Gets all the Entities.
         /// </summary>
@@ -40,6 +82,25 @@ namespace Pluton
                 ent.LookupShortPrefabName().Contains(name) ||
                 ent.name.Contains(name)
                 select new Entity(ent as BaseEntity)).ToList<Entity>();
+        }
+
+        public List<Entity> Locks()
+        {
+            return (from _lock in UnityEngine.Object.FindObjectsOfType<BaseLock>()
+                select new Entity(_lock as BaseEntity)).ToList<Entity>();
+        }
+
+        public List<Entity> LocksByAuthorizedUserID(ulong steamid)
+        {
+            return (from _lock in UnityEngine.Object.FindObjectsOfType<CodeLock>()
+                where ((List<ulong>)_lock.GetFieldValue("whitelistPlayers")).Contains(steamid)
+                select new Entity(_lock as BaseEntity)).ToList<Entity>();
+        }
+
+        public List<Entity> Loot()
+        {
+            return (from loot in UnityEngine.Object.FindObjectsOfType<LootContainer>()
+                select new Entity(loot as BaseEntity)).ToList<Entity>();
         }
 
         /// <summary>
@@ -87,6 +148,12 @@ namespace Pluton
                 where player.Name.Contains(nameorIP) ||
                 player.IP.Contains(nameorIP)
                 select player).ToList<Player>();
+        }
+
+        public List<Entity> Storage()
+        {
+            return (from storage in UnityEngine.Object.FindObjectsOfType<StorageContainer>()
+                select new Entity(storage as BaseEntity)).ToList<Entity>();
         }
     }
 }
