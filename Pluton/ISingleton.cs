1deffad33b2938db7688e02afeffbfc0d57de831
balu92 <balugyanszky.gabor@gmail.com>
2015-03-04 21:20:43 +0100

    Singleton

---
+++ Pluton/ISingleton.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pluton
+{
+    public interface ISingleton
+    {
+        void Initialize();
+    }
+}
+
---
+++ Pluton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pluton
+{
+    public abstract class Singleton<T> : CountedInstance where T : ISingleton
+    {
+        static T Instance;
+
+        public static T GetInstance()
+        {
+            return Singleton<T>.Instance;
+        }
+
+        static Singleton()
+        {
+            Singleton<T>.Instance = Activator.CreateInstance<T>();
+            Singleton<T>.Instance.Initialize();
+        }
+    }
+}
+
