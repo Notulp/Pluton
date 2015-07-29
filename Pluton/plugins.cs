efd5a414c88b27365ec97603e1d0a10ead012dc8
balu92 <balugyanszky.gabor@gmail.com>
2015-07-29 21:42:12 +0100

    readability

--- Pluton/plugins.cs
+++ Pluton/plugins.cs
@@ -16,7 +16,7 @@ namespace Pluton
             string result = String.Format("Loaded plugins({0}):\r\n", count);
             foreach (BasePlugin plugin in PluginLoader.GetInstance().Plugins.Values) {
                 result += String.Format("    {0, -22} [{1, -10}], timers: {2, 8}, parallel: {3, 8}\r\n", plugin.Name, plugin.Type, plugin.Timers.Count + plugin.ParallelTimers.Count, plugin.ParallelTimers.Count);
-                result += String.Format("Author: {0}, about: {1}, version: {2}\r\n", plugin.Author, plugin.About, plugin.Version);
+                result += String.Format("Author: {0}, about: {1}, version: {2}\r\n\r\n", plugin.Author, plugin.About, plugin.Version);
             }
             args.ReplyWith(result);
         }
