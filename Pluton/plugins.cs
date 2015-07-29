291cb4fd55cddf3ef5e821991c8dd02e2e1d416d
balu92 <balugyanszky.gabor@gmail.com>
2015-07-29 22:50:04 +0100

    newline

--- Pluton/plugins.cs
+++ Pluton/plugins.cs
@@ -9,10 +9,10 @@ namespace Pluton
         public static void Loaded(ConsoleSystem.Arg args)
         {
             int count = PluginLoader.GetInstance().Plugins.Count;
-            string result = String.Format("Loaded plugins({0}):\r\n", count);
+            string result = String.Format("Loaded plugins({0}):" + Environment.NewLine, count);
             foreach (BasePlugin plugin in PluginLoader.GetInstance().Plugins.Values) {
                 result += String.Format("    {0, -22} [{1, -10}], timers: {2, 8}, parallel: {3, 8}\r\n", plugin.Name, plugin.Type, plugin.Timers.Count + plugin.ParallelTimers.Count, plugin.ParallelTimers.Count);
-                result += String.Format("Author: {0}, about: {1}, version: {2}\r\n\r\n", plugin.Author, plugin.About, plugin.Version);
+                result += String.Format("Author: {0}, about: {1}, version: {2}" + Environment.NewLine + Environment.NewLine, plugin.Author, plugin.About, plugin.Version);
             }
             args.ReplyWith(result);
         }
