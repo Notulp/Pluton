8837a00831098d270bdf2065d937a64fdcef3f53
balu92 <balugyanszky.gabor@gmail.com>
2015-07-31 23:59:21 +0100

    BPDestroyedEvent naming conventions

--- Pluton/Events/BuildingPartDestroyedEvent.cs
+++ Pluton/Events/BuildingPartDestroyedEvent.cs
@@ -4,49 +4,46 @@ namespace Pluton.Events
 {
     public class BuildingPartDestroyedEvent : CountedInstance
     {
-        private BuildingPart buildingPart;
-        public readonly HitInfo _info;
+        public readonly BuildingPart BuildingPart;
+        [Obsolete("BuildingPartDestroyedEvent.buildingPart is obsolete and will be removed, please use BuildingPartDestroyedEvent.BuildingPart", true)]
+        public BuildingPart buildingPart {
+            get { return BuildingPart; }
+        }
+
+        public readonly HitInfo Info;
+        [Obsolete("BuildingPartDestroyedEvent._info is obsolete and will be removed, please use BuildingPartDestroyedEvent.Info", true)]
+        public HitInfo _info {
+            get { return Info; }
+        }
+
         public readonly string HitBone;
 
         public BuildingPartDestroyedEvent(BuildingBlock bb, HitInfo info)
         {
-            _info = info;
-            buildingPart = new BuildingPart(bb);
+            Info = info;
+            BuildingPart = new BuildingPart(bb);
             string bonename = StringPool.Get(info.HitBone);
             HitBone = bonename == "" ? "unknown" : bonename;
         }
 
-        public BuildingPart BuildingPart
-        {
-            get { return buildingPart; }
-        }
-
-        public float[] DamageAmounts
-        {
-            get
-            {
-                return _info.damageTypes.types;
+        public float[] DamageAmounts {
+            get {
+                return Info.damageTypes.types;
             }
-            set
-            {
-                _info.damageTypes.types = value;
+            set {
+                Info.damageTypes.types = value;
             }
         }
 
-        public Rust.DamageType DamageType
-        {
-            get { return _info.damageTypes.GetMajorityDamageType(); }
+        public Rust.DamageType DamageType {
+            get { return Info.damageTypes.GetMajorityDamageType(); }
         }
 
-        public Entity Attacker
-        {
-            get
-            {
-                try
-                {
-                    if (_info.Initiator != null)
-                    {
-                        BaseEntity ent = _info.Initiator;
+        public Entity Attacker {
+            get {
+                try {
+                    if (Info.Initiator != null) {
+                        BaseEntity ent = Info.Initiator;
                         BasePlayer p = ent.GetComponent<BasePlayer>();
                         if (p != null)
                             return Server.GetPlayer(p);
@@ -58,37 +55,29 @@ namespace Pluton.Events
                         return new Entity(ent);
                     }
                     return null;
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogWarning("[HurtEvent] Got an exception instead of the attacker.");
+                } catch (Exception ex) {
+                    Logger.LogWarning("[BPDestroyedEvent] Got an exception instead of the attacker.");
                     Logger.LogException(ex);
                     return null;
                 }
             }
         }
 
-        public InvItem Weapon
-        {
-            get
-            {
-                try
-                {
-                    if (_info.Weapon == null)
+        public InvItem Weapon {
+            get {
+                try {
+                    if (Info.Weapon == null)
                         return null;
-                    uint itemUID = (uint)_info.Weapon.GetFieldValue("ownerItemUID");
+                    uint itemUID = (uint)Info.Weapon.GetFieldValue("ownerItemUID");
 
-                    BasePlayer ownerPlayer = _info.Weapon.ownerPlayer;
-                    if (ownerPlayer == null)
-                    {
+                    BasePlayer ownerPlayer = Info.Weapon.ownerPlayer;
+                    if (ownerPlayer == null) {
                         return null;
                     }
 
                     return new InvItem(ownerPlayer.inventory.FindItemUID(itemUID));
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogWarning("[HurtEvent] Got an exception instead of the weapon.");
+                } catch (Exception ex) {
+                    Logger.LogWarning("[BPDestroyedEvent] Got an exception instead of the weapon.");
                     Logger.LogException(ex);
                     return null;
                 }
