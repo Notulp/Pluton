23ba9d4773264f8c8b8fbd602a801c3dd0e6718d
dretax <dretax@users.noreply.github.com>
2015-10-11 22:18:23 +0200

    Make these voids return the entity instead

--- Pluton/World.cs
+++ Pluton/World.cs
@@ -91,16 +91,18 @@ namespace Pluton
             AirDropAt(player.Location, speed, height);
         }
         
-        public void PatrolHelicopter(float height = 10f)
+        public Entity PatrolHelicopter(float height = 10f)
         {
             BaseEntity baseEntity = GameManager.server.CreateEntity("assets/bundled/prefabs/npc/patrol_helicopter/PatrolHelicopter.prefab", default(Vector3), default(Quaternion), true);
             if (baseEntity)
             {
                 baseEntity.Spawn(true);
+                return new Entity(baseEntity)
             }
+            return null;
         }
 
-        public void PatrolHelicopterAt(Vector3 position, float height = 10f)
+        public Entity PatrolHelicopterAt(Vector3 position, float height = 10f)
         {
             BaseEntity baseEntity = GameManager.server.CreateEntity("assets/bundled/prefabs/npc/patrol_helicopter/PatrolHelicopter.prefab", default(Vector3), default(Quaternion), true);
             if (baseEntity)
@@ -108,17 +110,19 @@ namespace Pluton
                 PatrolHelicopterAI component = baseEntity.GetComponent<PatrolHelicopterAI>();
                 component.SetInitialDestination(position + new Vector3(0, height, 0), 0.25f);
                 baseEntity.Spawn(true);
+                return new Entity(baseEntity);
             }
+            return null;
         }
 
-        public void PatrolHelicopterAt(float x, float y, float z, float height = 10f)
+        public Entity PatrolHelicopterAt(float x, float y, float z, float height = 10f)
         {
-            PatrolHelicopterAt(new Vector3(x, y, z), height);
+            return PatrolHelicopterAt(new Vector3(x, y, z), height);
         }
 
-        public void PatrolHelicopterAtPlayer(Player player, float height = 10f)
+        public Entity PatrolHelicopterAtPlayer(Player player, float height = 10f)
         {
-            PatrolHelicopterAt(player.Location, height);
+            return PatrolHelicopterAt(player.Location, height);
         }
 
         public float GetGround(float x, float z)
