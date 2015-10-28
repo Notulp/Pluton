65c0b08c38287328336d3c7ea4276da5ef2fc1a3
balu92 <balugyanszky.gabor@gmail.com>
2015-10-28 13:04:24 +0000

    doc for generation init

---
+++ Pluton/Documentation/DoqAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluton.Documentation
+{
+    public class DoqAttribute : Attribute
+    {
+        public object category;
+
+        public object doqType;
+
+        public string description = "none";
+
+        public Type[] args;
+
+        public DoqAttribute(params Type[] Args){
+            args = Args;
+        }
+    }
+}
+
---
+++ Pluton/Documentation/DoqEnums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pluton.Documentation
+{
+    public enum DoqCategory
+    {
+        Pluton_Hooks,
+        Pluton_Objects,
+        Pluton_Events,
+        Pluton_Libraries,
+        Pluton_Libary_Methods
+    }
+
+    public enum DoqType
+    {
+        Class,
+        Field,
+        Enum,
+        Hook,
+        Method
+    }
+}
+
